7d313cc7e02f9dbc4f32d82194405b0ea2bbb28d
jan9 <edujuhye@gmail.com>
2021-05-18 13:35:00 -0700

    added navbar component

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './app.css';
+import NavBar from './components/NavBar';
 import Habits from './components/Habits';
 
-function App() {
-  return <Habits></Habits>;
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: 'Reading', count: 0 },
+      { id: 2, name: 'Dancing', count: 0 },
+      { id: 3, name: 'Coding', count: 0 },
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits: habits });
+  };
+
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits: habits });
+  };
+
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+  };
+  render() {
+    return (
+      <>
+        <NavBar
+          totalCount={this.state.habits.filter((item) => item.count > 0).length}
+        />
+
+        <Habits
+          habits={this.state.habits}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
